a3ac85319304a8c23e17f616f07f0f3e62721909
Ber1709 <85528091+Ber1709@users.noreply.github.com>
2023-11-06 18:02:23 -0400

    Modulos de Requisiciones y Ordenes de Compra

---
+++ TriadRestockSystem/Controllers/OrdenesCompraController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TriadRestockSystem.Security;
+using TriadRestockSystemData.Data;
+
+namespace TriadRestockSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [JwtData]
+    [Authorize(Roles = RolesNames.ADMINISTRADOR + "," + RolesNames.COMPRAS)]
+    public class OrdenesCompraController : ControllerBase
+    {
+        private readonly InventarioDBContext _db;
+        private readonly IConfiguration _configuration;
+
+        public OrdenesCompraController(InventarioDBContext db, IConfiguration configuration)
+        {
+            _db = db;
+            _configuration = configuration;
+
+        }
+
+        [HttpGet("getOrdenesCompra")]
+        public ActionResult GetOrdenesCompra()
+        {
+            var result = _db.OrdenesCompras
+                .Include(a => a.IdAlmacenNavigation)
+                .Include(a => a.IdProveedorNavigation)
+                .Include(a => a.IdDocumentoNavigation)
+                .Select(x => new
+                {
+                    Key = x.IdOrdenCompra,
+                    Id = x.IdOrdenCompra,
+                    Almacen = x.IdAlmacen,
+                    Proveedor = x.IdProveedor,
+                    Documento = x.IdDocumento,
+                    x.SubTotal,
+                    x.TotalImpuestos,
+                    x.Total,
+
+                })
+                .ToList();
+            return Ok(result);
+        }
+
+        [HttpGet("getRequisiciones")]
+        public ActionResult GetRequisicion()
+        {
+            var result = _db.Requisiciones
+                .Include(a => a.IdDocumentoNavigation)
+                 .Select(x => new
+                 {
+                     Key = x.IdRequisicion,
+                     Id = x.IdRequisicion,
+               
+                 })
+                 .ToList();
+            return Ok(result);
+        }
+    }
+}
